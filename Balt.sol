623d84ee2848f18f56c12737c4b517054592274f
Thibault Langlois-Berthelot <44510413+KSTLB69@users.noreply.github.com>
2019-07-30 16:43:58 +0200

    Create Balt.sol

---
+++ Balt.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.5.1;
+
+import "./SafeMath.sol";
+
+/*
+Person n°1
+*/
+
+
+contract Balt{
+  
+  using SafeMath for uint256;
+  address payable public beneficiary;
+  uint256 public releaseTime;
+
+  constructor(address payable _beneficiary, uint256 _releaseTime) public payable{
+    require(_releaseTime > block.timestamp);
+    beneficiary = _beneficiary;
+    releaseTime = _releaseTime;
+  }
+
+  function release() public {
+    require(block.timestamp >= releaseTime);
+    address(beneficiary).transfer(address(this).balance);
+  }
+  
+}
