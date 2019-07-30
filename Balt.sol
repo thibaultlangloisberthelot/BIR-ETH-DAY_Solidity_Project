92d046480f99720013956806a05c8294e46d8aff
Thibault Langlois-Berthelot <44510413+KSTLB69@users.noreply.github.com>
2019-07-30 20:32:58 +0200

    Update Balt.sol

--- Balt.sol
+++ Balt.sol
@@ -3,7 +3,15 @@ pragma solidity ^0.5.1;
 import "./SafeMath.sol";
 
 /*
-Person n°1 - 13/04/2007
+Those (smart) contracts (2) will be use to send automaticaly some eth, to a specific date and address, after a certain amount of time. I will use it as a "saving account" for some of my familly members (2).
+
+Here are some features of those (2) contract :
+
+There is one contract for each person.
+Only the beneficiary can withdraw the funds.
+Everyone can deposit fund on the smart contract, at any time (Happy Birthday).
+The funds can be withdraw only after the releaseTime.
+The releaseTime correspond to the birthday date of the beneficiary : try to guess it if you want ;-)
 */
 
 
