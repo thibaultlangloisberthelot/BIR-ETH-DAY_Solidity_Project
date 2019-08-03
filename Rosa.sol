9e14446ed209dd1858e027c9611a48eda4217c28
Thibault Langlois-Berthelot <44510413+KSTLB69@users.noreply.github.com>
2019-08-03 17:44:16 +0200

    Update Rosa.sol

--- Rosa.sol
+++ Rosa.sol
@@ -1,1 +1,39 @@
-12/04/2009
+pragma solidity ^0.5.1;
+
+import "./SafeMath.sol";
+
+contract Rosa{
+  
+  
+  using SafeMath for uint256;
+  address payable public beneficiary;
+  uint256 public releaseTime;
+
+  modifier OnlyRosa { require (msg.sender == //adress );
+  _;
+  }
+
+  function () external payable {
+  }
+
+  function getBalance()  public view returns (uint) {
+    return address(this).balance;
+  }
+    
+  constructor(
+    address payable _beneficiary,
+    uint256 _releaseTime
+  )
+    public
+    payable
+  {
+    require(_releaseTime > block.timestamp);
+    beneficiary = _beneficiary;
+    releaseTime = _releaseTime;
+  }
+
+  function release() public {
+    require(block.timestamp >= releaseTime);
+    address(beneficiary).transfer(address(this).balance);
+  }
+}
